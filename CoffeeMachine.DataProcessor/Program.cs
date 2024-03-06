0817ade3c43a8f1030f91c34427e31b90e1c4368
Michelle <rotariumihaela.bv@gmail.com>
2024-03-06 23:42:33 +0200

    Add project files.

---
+++ CoffeeMachine.DataProcessor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeMachine.DataProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
